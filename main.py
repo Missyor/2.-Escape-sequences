01187f1bc68f0b2b6b33beda4293c3345d0af438
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 15:08:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#print("nelson mandela)
+print("Nelson mandela,")
+print("is dead")
+print("\n")
+
+#escape sequences help format your string
+print("Nelson mandela, \nis dead")
+print("\n")
+
+print("MLK Jr. said \"I have a dream\" in his famous speech")
+print("\n")
+
+#The slashes are normally forward but for practice we will do them backwards
+print("https:\\www.google.com\\search")
